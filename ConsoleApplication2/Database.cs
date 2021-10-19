1609e71992d66aac4608b0b9975ebe4740172e80
AiltonSilvaSC <ailtonsscarvalho@gmail.com>
2021-10-19 18:56:13 -0300

    Criar classe database

---
+++ ConsoleApplication2/Database.cs
@@ -0,0 +1,24 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+  public class Database
+  {
+    private string ConnectionString { get; set; }
+
+    public Database(string connectionString)
+    {
+      this.ConnectionString = connectionString;
+    }
+
+    public void OpenConnection()
+    {
+      NpgsqlConnection conn = new NpgsqlConnection(this.ConnectionString);
+      conn.Open();
+    }
+  }
+}
