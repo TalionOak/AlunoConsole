79e9c938f99810d9ae59b36f6dc59c44906f9cc4
AiltonSilvaSC <ailtonsscarvalho@gmail.com>
2021-10-20 11:27:04 -0300

    Adicionar Aluno

---
+++ ConsoleApplication2/Aluno.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApplication1
+{
+  public class Aluno
+  {
+    public int CodAluno { get; set; }
+    public string Nome { get; set; }
+    public string CPF { get; set; }
+    public string Nascimento { get; set; }
+    public string Telefone { get; set; }
+    public string Endereco { get; set; }
+    public string Email { get; set; }
+    public string CodigoPostal { get; set; }
+    public string Bairro { get; set; }
+
+    public string ResponsavelCPF { get; set; }
+    public string ResponsavelNascimento { get; set; }
+
+  }
+}
